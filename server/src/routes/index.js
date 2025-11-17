31e1c352ad401534cc2b73409a9b8f43e3f6f187
QUBITABHAY <codingcontestabhay@gmail.com>
2025-11-17 21:42:26 +0530

    add event route

--- server/src/routes/index.js
+++ server/src/routes/index.js
@@ -1,8 +1,10 @@
 import { Router } from "express";
 import userRoutes from "../users/userRoutes.js";
+import eventRoutes from "../event/eventRoutes.js";
 
 const routes = Router();
 
 routes.use("/api/user", userRoutes);
+routes.use("/api/event", eventRoutes);
 
 export default routes;
